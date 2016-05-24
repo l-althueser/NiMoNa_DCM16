77f0338fe345b1a537548f4e2c0a86b1269e446b
Lutz Althüser <coding@l-althueser.de>
2016-05-24 17:31:32 +0200

    Euler 1O is working

--- numerical_examples/Euler_1O.py
+++ numerical_examples/Euler_1O.py
@@ -4,22 +4,15 @@
 #	Euler_1O.py
 #------------------------------------------------------------------------------
 # description    :Implementation eines Euler Verfahrens 1. Ordnung (1O).
-# author         :	l-althueser
-#				t-wiedemann
 #
 # usage          :
 # python_version :3.5.1
 #
-# changes/notes  :20160425 :
 #==============================================================================
 
-# Explizites Euler-Verfahren 1. Ordnung
-# Merke Euler-Verfahren = RK1
-# y' = f0(t,y)
-# y(t_n+1) = y_n+1 = y_n + h * f(t_n,y_n)
+import numpy as np
 
-
-def Euler_1O(f,t,t_k,y,n):
+def Euler_1O(f,t_0,t_k,y_0,n):
 	# f  : gegeben als y' = f(t,y)
 	# t_0: Startwert für t (initial value)
 	# t_k: Endwert für t (final value)
@@ -28,10 +21,12 @@ def Euler_1O(f,t,t_k,y,n):
 	# h  : Diskretisierungs-Schrittweite mit h > 0
 	# Gibt eine Matrix von angenommenden Werte zurück.
 
-    h = (t_k - t[:,0]) / float(n)
-
-    for i in range(n):			
-        y[:,i+1] = y[:,i] + h * f(t[:,i], y[:,i])
-        t[:,i+1] = t[:,i] + h
+	# Erstelle Matrix der Größe n
+	E = np.hstack((np.vstack((t_0,y_0)),np.zeros((2,n))))
+	h = (t_k - t_0) / float(n)
+	
+	for i in range(n):			
+		E[1,i+1] = E[1,i] + h * f(E[0,i], E[1,i])	#y
+		E[0,i+1] = E[0,i] + h						#t
 		
-    return y 
+	return E
