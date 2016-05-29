a3445e58bf01e69381d49bdeb7c34a59282ff89d
t-wiedemann <Timo@Timos-MacBook-Pro.local>
2016-05-29 20:50:08 +0200

    Vernünftige Implementierung des Beispiels
    Ich habe auch versucht das Lineare Modell zu berücksichtigen. Habe
    einfach B=0 gesetzt (Bei Christian C). Das was da raus ist ergibt auch
    Sinn, aber sieht leider nicht so aus, wie bei Christian. Der hatte da
    ja ein Gauß angemalt, wenn ich das Bild in whatsapp richtig deute. vlld
    könnt ihr ja mal drüber schauen. Wie gesagt, die Grafik zu dem Beispiel
    ist jetzt online. Aber die Plots sollten auch einiges verständlicher
    machen. Naja ich mache für heute Schluss..

--- DCM/hemodynamicExample-1-exe.py
+++ DCM/hemodynamicExample-1-exe.py
@@ -22,7 +22,7 @@ from programs import bilinearModel as BM
 
 #-----------------------------------------------------------------------------------------------------------------
 # Parameter Beispiel 1
-T = 20.                         # Endzeit
+T = 100.                         # Endzeit
 t0 = 0.                         # Anfangszeit
 dt = 0.1                        # Zeitschrittlaenge         
 t = np.arange(t0,T+dt,dt)       # Zeitarray
@@ -39,7 +39,11 @@ C = np.array([[1, 0],[0, 0],[0, 0]])                        # äußerer Einfluss
 
 # äußerer Stimulus
 u = np.zeros((len(B), len(t)))             
-u[:,51] = 1.
+u[0,101:-200:200] = 10.         # Stimulus u1   
+
+u[1,451:550] = 2.              # Stimulus u2 
+u[1,251:350] = 5.              # Stimulus u2
+u[1, 691:710] = 2.             # Stimulus u2
 
 # Anfangsbedingunden  
 x_0 = np.ones(15)
@@ -58,34 +62,195 @@ x = RK4.RK4(HM.stateEquations,theta,u,x_0,t0,T,dt)      # Lösung mithilfe des R
 
 y = HM.BOLDsignal(x)                                    # Berechnung des BOLD-Signals
 
-plt.figure()
+
+plt.rcParams['figure.figsize'] = (15.0, 10.0) # Fenstergröße anpassen
+
+
+
+#-----------------------------------------------------------------------------------------------------------------
+# Plotten
+
+#-------------------------- BOLD ------------------------------------
+f1 = plt.figure(1) 
+f1.suptitle('Bilineares Modell', fontsize = 20)
+# Stimulus 
+ax1 = plt.subplot(311)
+ax1.tick_params(width = 1)
+plt.plot(t,u[0,:])
+plt.setp(ax1.get_xticklabels(), visible=False)
+plt.ylabel('$u_1(t)$', fontsize = 16.)
+plt.title('Stimuli')
+
+ax2 = plt.subplot(312,sharex = ax1, sharey =ax1)
+ax2.tick_params(width = 1)
+plt.plot(t,u[1,:])
+ax2.set_ylim([0,np.max(u)+1])
+plt.setp(ax2.get_xticklabels(), visible=False)
+plt.ylabel('$u_2(t)$', fontsize = 16.)
+
+# Signal Plotten
+ax3 = plt.subplot(313,sharex = ax1)
+plt.setp(ax3.get_xticklabels(), fontsize = 14.)
+plt.xticks(np.arange(10,110,10))
+ax3.tick_params(width = 1)
 
 # Region 1:
-#plt.plot(t,x[0,:],'r',label='Region 1')     #Gehirnaktivität
+plt.plot(t,y[0,:],'r',label='Region 1')      #BOLD-Signal
+# Region 2:
+plt.plot(t,y[1,:],'g',label='Region 2')      #BOLD-Signal
+#Region 3:
+plt.plot(t,y[2,:],'b',label='Region 3')      #BOLD-Signal
+
+plt.legend()
+plt.xlabel('Zeit t', fontsize = 14.)
+plt.ylabel('$y(t)$', fontsize = 16.)
+plt.title('BOLD-Signal nach Region')
+
+#-------------------------- Gehirnaktivität ------------------------------------
+f2 = plt.figure(2)
+f2.suptitle('Bilineares Modell', fontsize = 20)
+# Stimulus plotten
+ax4 = plt.subplot(311)
+ax4.tick_params(width = 1)
+plt.plot(t,u[0,:])
+plt.setp(ax4.get_xticklabels(), visible=False)
+plt.ylabel('$u_1(t)$', fontsize = 16.)
+plt.title('Stimuli')
+
+
+ax5 = plt.subplot(312,sharex = ax4, sharey =ax4)
+ax5.tick_params(width = 1)
+plt.plot(t,u[1,:])
+ax5.set_ylim([0,np.max(u)+1])
+plt.setp(ax5.get_xticklabels(), visible=False)
+plt.ylabel('$u_2(t)$', fontsize = 16.)
+
+
+# Gehirnaktivität plotten
+ax6 = plt.subplot(313,sharex = ax4)
+plt.setp(ax6.get_xticklabels(), fontsize = 14.)
+plt.xticks(np.arange(10,110,10))
+ax6.tick_params(width = 1)
+
+# Region 1:
+plt.plot(t,x[0,:],'r',label='Region 1')     #Gehirnaktivität
 #plt.plot(t,x[3,:],'r',label='Region 1')     #Vasodilatorisches Signal
 #plt.plot(t,x[6,:],'r',label='Region 1')     #Blutfluss 
 #plt.plot(t,x[9,:],'r',label='Region 1')     #Blutvolumen
 #plt.plot(t,x[12,:],'r',label='Region 1')    #Deoxyhemoglobingehalt
-plt.plot(t,y[0,:],'r',label='Region 1')      #BOLD-Signal
 
 # Region 2:
-#plt.plot(t,x[1,:],'g',label='Region 2')     #Gehirnaktivität
+plt.plot(t,x[1,:],'g',label='Region 2')     #Gehirnaktivität
 #plt.plot(t,x[4,:],'g',label='Region 2')     #Vasodilatorisches Signal
 #plt.plot(t,x[7,:],'g',label='Region 2')     #Blutfluss
 #plt.plot(t,x[10,:],'g',label='Region 2')    #Blutvolumen
 #plt.plot(t,x[13,:],'g',label='Region 2')    #Deoxyhemoglobingehalt
-plt.plot(t,y[1,:],'g',label='Region 2')      #BOLD-Signal
 
-#R egion 3:
-#plt.plot(t,x[2,:],'b',label='Region 3')     #Gehirnaktivität
+# Region 3:
+plt.plot(t,x[2,:],'b',label='Region 3')     #Gehirnaktivität
 #plt.plot(t,x[5,:],'b',label='Region 3')     #Vasodilatorisches Signal
 #plt.plot(t,x[8,:],'b',label='Region 3')     #Blutfluss 
 #plt.plot(t,x[11,:],'b',label='Region 3')    #Blutvolumen
 #plt.plot(t,x[14,:],'b',label='Region 3')    #Deoxyhemoglobingehalt
-plt.plot(t,y[2,:],'b',label='Region 3')      #BOLD-Signal
-
-plt.title('BOLD-Signal nach Region')
 plt.legend()
-plt.xlabel('Zeit t')
-plt.show
+plt.xlabel('Zeit t', fontsize = 14.)
+plt.ylabel('$z(t)$', fontsize = 16.)
+plt.title('Gehirnaktivität nach Region')
+#plt.show()
+
+#--------------------------------------------------------------------- Lineares Modell zum Vergleich -----------------------------------------------
+# Änderung der Anfagsparameter B=0
+Blin = np.array([np.zeros((3,3))])
+thetalin = np.array([A,Blin,C])
+
+#Simulation
+xlin = RK4.RK4(HM.stateEquations,thetalin,u,x_0,t0,T,dt)      # Lösung mithilfe des RK4-Verfahrens
+ylin = HM.BOLDsignal(xlin)                                    # Berechnung des BOLD-Signals
+
+# Plotten
+
+#-------------------------- BOLD ------------------------------------
+f3 = plt.figure(3) 
+f3.suptitle('Lineares Modell', fontsize = 20)
+# Stimulus 
+ax1lin = plt.subplot(311)
+ax1lin.tick_params(width = 1)
+plt.plot(t,u[0,:])
+plt.setp(ax1lin.get_xticklabels(), visible=False)
+plt.ylabel('$u_1(t)$', fontsize = 16.)
+plt.title('Stimuli')
+
+ax2lin = plt.subplot(312,sharex = ax1lin, sharey =ax1lin)
+ax2lin.tick_params(width = 1)
+plt.plot(t,u[1,:])
+ax2lin.set_ylim([0,np.max(u)+1])
+plt.setp(ax2lin.get_xticklabels(), visible=False)
+plt.ylabel('$u_2(t)$', fontsize = 16.)
+
+# Signal Plotten
+ax3lin = plt.subplot(313,sharex = ax1lin)
+plt.setp(ax3lin.get_xticklabels(), fontsize = 14.)
+plt.xticks(np.arange(10,110,10))
+ax3lin.tick_params(width = 1)
+
+# Region 1:
+plt.plot(t,ylin[0,:],'r',label='Region 1')      #BOLD-Signal
+# Region 2:
+plt.plot(t,ylin[1,:],'g',label='Region 2')      #BOLD-Signal
+#Region 3:
+plt.plot(t,ylin[2,:],'b',label='Region 3')      #BOLD-Signal
+
+plt.legend()
+plt.xlabel('Zeit t', fontsize = 14.)
+plt.ylabel('$y(t)$', fontsize = 16.)
+plt.title('BOLD-Signal nach Region')
+
+#-------------------------- Gehirnaktivität ------------------------------------
+f4 = plt.figure(4)
+f4.suptitle('Lineares Modell', fontsize = 20)
+# Stimulus plotten
+ax4lin = plt.subplot(311)
+ax4lin.tick_params(width = 1)
+plt.plot(t,u[0,:])
+plt.setp(ax4lin.get_xticklabels(), visible=False)
+plt.ylabel('$u_1(t)$', fontsize = 16.)
+plt.title('Stimuli')
+
+
+ax5lin = plt.subplot(312,sharex = ax4lin, sharey =ax4lin)
+ax5lin.tick_params(width = 1)
+plt.plot(t,u[1,:])
+ax5lin.set_ylim([0,np.max(u)+1])
+plt.setp(ax5lin.get_xticklabels(), visible=False)
+plt.ylabel('$u_2(t)$', fontsize = 16.)
+
+
+# Gehirnaktivität plotten
+ax6lin = plt.subplot(313,sharex = ax4lin)
+plt.setp(ax6lin.get_xticklabels(), fontsize = 14.)
+plt.xticks(np.arange(10,110,10))
+ax6lin.tick_params(width = 1)
+
+# Region 1:
+plt.plot(t,xlin[0,:],'r',label='Region 1')     #Gehirnaktivität
+# Region 2:
+plt.plot(t,xlin[1,:],'g',label='Region 2')     #Gehirnaktivität
+# Region 3:
+plt.plot(t,xlin[2,:],'b',label='Region 3')     #Gehirnaktivität
+
+plt.legend()
+plt.xlabel('Zeit t', fontsize = 14.)
+plt.ylabel('$z(t)$', fontsize = 16.)
+plt.title('Gehirnaktivität nach Region')
+plt.show()
+
+
+
+
+
+
+
+
+
+
 
